c951f1e0e4fc0cbbe2a331a37031ae10ff701091
BrayannDEV <142523285+BrayannDEV@users.noreply.github.com>
2024-11-07 20:39:30 -0300

    calendario
    Update de calendario que não deu certo

--- src/app/admin/agendamento/page.js
+++ src/app/admin/agendamento/page.js
@@ -5,14 +5,41 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
 
+
 export default function Agendamento() {
 
+    async function Calendario() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendario(calendarEl, {
+        locale: 'pt-br',
+        timeZone: 'America/Sao_Paulo',
+        buttonText: {
+            today: 'hoje',
+            month: 'Mes',
+            week: 'Semana',
+            day: 'dia',
+            list: 'lista'
+        },
+        initialView: 'dayGridMonth'
+        });
+        calendar.render();
+    }
+    
+
     return(
-        <FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" 
-         events={[
-          { title: 'event 1', date: '2024-11-07' },
-          { title: 'event 2', date: '2024-11-20' }
-        ]}/>
+        <div>
+            <div>
+                <button className="btn btn-primary mt-3" onClick={Calendario} style={{backgroundColor: "DF808F", border: "none"}}>Ver Calendário</button>
+            </div>
+            <FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" selectable="true"
+            events={[
+            { title: 'Maria', start: '2024-11-07T08:30:00', end: '2024-11-07T09:30:00', display:'backgroud' },
+            { title: 'Julia', date: '2024-11-20' }
+            ]}/>
+            
+        </div>
+        
+        
     );
 
 }
