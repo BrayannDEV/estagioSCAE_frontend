a325082354abc3a48f0f702aa85bd65a5da398bd
BrayannDEV <brayannrlima100@gmail.com>
2024-11-17 02:52:25 -0300

    Agendamento Publico
    Criando agendamento para clientes

--- src/app/agendamento/page.js
+++ src/app/agendamento/page.js
@@ -1,34 +1,188 @@
 'use client'
+import { useRef, useState, useEffect } from "react"
+import httpClient from "../utils/httpClient.js";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+import interactionPlugin from "@fullcalendar/interaction"
 
 export default function Agendamento() {
     
+    let data = useRef("");
+    let horaInicial = useRef("");
+    let cliente = useRef("");
+    let procedimento = useRef("");
+
+    async function cadastrar(){
+    
+        
+        let procedimentoSelecionado = listaProcedimentos.find(p => p.id == procedimento.current.value);
+        if (procedimentoSelecionado) {
+
+            //Convertendo o tempo do procedimento para horas e somando com a hora inicial para virar a hora final
+            let minutosTotal = parseInt(procedimentoSelecionado.tempo, 10);
+            let horas = Math.floor(minutosTotal / 60);
+            let minutos = minutosTotal % 60;
+            let [horaInicialHoras, horaInicialMinutos] = horaInicial.current.value.split(":").map(Number);
+            let horaFinalDate = new Date();
+            horaFinalDate.setHours(horaInicialHoras + horas);
+            horaFinalDate.setMinutes(horaInicialMinutos + minutos);
+            let horaFinal = horaFinalDate.toTimeString().slice(0, 5);
+
+            let agenda = {
+            data: data.current.value,
+            horaInicial: horaInicial.current.value,
+            horaFinal: horaFinal,
+            cliente: cliente.current.value,
+            procedimento: procedimento.current.value,
+            };
+        
+            try {
+                
+            const result = await httpClient.post("/agenda", agenda)
+            console.log("agenda", agenda);
+            
+            data.current.value = "";
+            horaInicial.current.value = "";
+            cliente.current.value = "";
+            procedimento.current.value = "";
+            alert("Cadastrado com sucesso!")
+        
+            let ok = result.status == 201;
+        
+            } catch (erro) {
+            console.log(erro);
+            }
+        } else {
+            console.log("Procedimento não encontrado"); alert("Procedimento não encontrado"); 
+        }
+    }
+
+    let [listaProcedimentos, setListaProcedimentos] = useState([]);
+    useEffect((e) => {
+        carregarProcedimentos();
+    }, [])
+
+    async function carregarProcedimentos() {
+        
+        try {
+            const result = await httpClient.get("/procedimento")
+            setListaProcedimentos(result)
+      
+            let ok = r.status == 201;
+      
+        } catch (erro) {
+            console.log(erro);
+        }
+    }
+
+
+    let [listaCliente, setListaCliente] = useState([]);
+    useEffect((e) => {
+        carregarCliente();
+    }, [])
+
+    async function carregarCliente() {
+        
+        try {
+            const result = await httpClient.get("/cliente")
+            setListaCliente(result)
+      
+            let ok = r.status == 201;
+      
+        } catch (erro) {
+            console.log(erro);
+        }
+    }
+
+
+    let [listaAgenda, setListaAgenda] = useState([]);
+    useEffect((e) => {
+      carregarAgenda();
+    }, [])
+
+    async function carregarAgenda() {
+        
+        try {
+          const result = await httpClient.get(`/agenda`)    
+          console.log(result);   
+          setListaAgenda(result)        
+      
+        }catch (erro) {
+          console.log(erro);
+        }
+    }
 
     return(
-        <section id="appointment" className="jarallax" style={{backgroundImage: "url(images/background-1.jpg)"}} >
-        {/* style="background-image: url(images/background-1.jpg); background-repeat: no-repeat; background-position: center;" */}
-        <div className="container-lg padding-medium">
-            <div className="offset-md-3 col-md-6 text-center ">
+        <div>
+            <section id="appointment" className="jarallax" style={{backgroundImage: "url(../../images/background-1.jpg)"}} >
+                <div className="container-lg padding-medium">
+                    <div className="offset-md-3 col-md-6 text-center ">
 
-            <h2 className="display-4 fw-normal mb-3">Agendar</h2>
+                        <h2 className="display-4 fw-normal mb-3">Agendar</h2>
 
-            <form className="contact-form row mt-5">
-                <div className="form-input col-lg-12 d-md-flex mb-3">
-                <input type="text" name="procedimento" placeholder="Procedimento" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3"/>
-                </div>
-                <div className="form-input col-lg-12 d-md-flex mb-3">
-                <input type="date" name="data" placeholder="Insira sua senha" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3"/>
-                <input type="time" name="hora" placeholder="Insira sua senha" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3"/>
-                </div>
-                <div class="col-md-12 col-sm-12 mb-4">
-                    <textarea type="text" name="observacao" placeholder="Escreva uma observação" rows="4" className="w-100 border-0 ps-4 py-2"></textarea>
-                </div>
-            </form>
+                        <form className="contact-form row mt-5">
+                            <div className="form-input col-lg-12 d-md-flex mb-3">
+                                <label className=" rounded-0 border-0 py-3 mb-2 me-3" for="procedimento">Procedimentos: </label>
+                                <select type="text" ref={procedimento} id="procedimento" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3">
+                                {listaProcedimentos.map(procedimento => (<option value={procedimento.id}>{procedimento.nome}</option>))}
+                                </select>
+                            </div>
+                            <div className="form-input col-lg-12 d-md-flex mb-3">
+                                <label className=" rounded-0 border-0 py-3 mb-2 me-3" for="cliente">Cliente: </label>
+                                <select type="text" ref={cliente} id="cliente" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3">
+                                {listaCliente.map(cliente => (<option value={cliente.id}>{cliente.nome}</option>))}
+                                </select>
+                            </div>
+                            <div className="form-input col-lg-12 d-md-flex mb-3">
+                                <input type="date" ref={data} name="data" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3"/>
+                                <input type="time" ref={horaInicial} name="hora" className="form-control w-100 rounded-0 border-0 ps-4 py-3 mb-2 me-3"/>
+                            </div>
+                        </form>
+                        <button className="btn btn-primary mt-3" onClick={cadastrar} style={{backgroundColor: "DF808F", border: "none"}}>Confirmar</button>
+                    </div>
+                </div>   
+            </section>
+            <div>
+                <FullCalendar plugins={[ dayGridPlugin, interactionPlugin  ]} initialView="dayGridMonth" selectable="true" 
+                events={
+                    listaAgenda.map(agenda =>{
 
-            <button className="btn btn-primary mt-3" style={{backgroundColor: "DF808F", border: "none"}}>Confirmar</button>
+                        let horaInicial = new Date(agenda.data);
+                        let [horas, minutos, segundos] = agenda.horaInicial.split(':');
+                        horaInicial.setHours(horas);
+                        horaInicial.setMinutes(minutos);
+                        horaInicial.setSeconds(segundos);
+
+                        let horaFinal = new Date(agenda.data);
+                        let [horasfinal, minutosfinal, segundosfinal] = agenda.horaFinal.split(':');
+                        horaFinal.setHours(horasfinal);
+                        horaFinal.setMinutes(minutosfinal);
+                        horaFinal.setSeconds(segundosfinal);
+
+
+                        return {
+                            title: "Indisponível",
+                            display: agenda.procedimento.nome,
+                            date: agenda.data,
+                            start: horaInicial,
+                            end: horaFinal
+                        }
+                    })
+                }
+                eventClick=
+                {
+                    function (info) {
+                        alert(`
+                            Horário indisponível das:
+                            ${(info.event.start).getHours()}:${String((info.event.start).getMinutes()).padStart(2, '0')} Até ${(info.event.end).getHours()}:${String((info.event.end).getMinutes()).padStart(2, '0')}
+                        `)
+                    }
+                }/>
+                
             </div>
-
         </div>
-        </section>
+        
     )
 
 }
